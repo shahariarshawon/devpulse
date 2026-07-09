3df2e88954b873e3f5aacf5af3f5b17bf5618e30
shahariarshawon <shahariararafat7739@gmail.com>
2026-07-09 23:41:10 +0600

    updated app for adding the error handler

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
-import express, { Application } from "express";
+import express from "express";
 import cors from "cors";
+import { errorHandler } from "./middleware/error.middleware";
 
-const app:Application = express();
-
+const app = express();
 
 app.use(cors());
 
 app.use(express.json());
 
-
-app.get("/", (req, res)=>{
-    res.json({
-        success:true,
-        message:"DevPulse API is running"
-    });
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "DevPulse API is running",
+  });
 });
 
+app.use(errorHandler);
 
 export default app;
