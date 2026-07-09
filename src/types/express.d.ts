1d396b675fd6f8bc45fab069395d70e24aa399d6
shahariarshawon <shahariararafat7739@gmail.com>
2026-07-09 23:58:38 +0600

    added types

---
+++ src/types/express.d.ts
@@ -0,0 +1,15 @@
+import { JwtPayload } from "jsonwebtoken";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: JwtPayload & {
+        id: number;
+        name: string;
+        role: "contributor" | "maintainer";
+      };
+    }
+  }
+}
+
+export {};
